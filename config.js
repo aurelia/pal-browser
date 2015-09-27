566843d17d10b58a79dc3395e3640bf737e1b478
Rob Eisenberg <rob@bluespire.com>
2015-09-27 17:01:21 -0400

    chore(package): add aurelia-pal dependency

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ System.config({
   },
 
   map: {
+    "aurelia-pal": "github:aurelia/pal@0.1.1",
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@1.2.0",
