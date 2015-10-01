a6961f33bf6fae7f8cdaeb235e5f5aca33aff5fe
Rob Eisenberg <rob@bluespire.com>
2015-10-01 09:32:44 -0400

    fix(html-template-element): correctly handle child templates

--- src/html-template-element.js
+++ src/html-template-element.js
@@ -29,13 +29,24 @@ function fixHTMLTemplateElement(template) {
   let child;
 
   while (child = template.firstChild) {
-    if (child.tagName === 'TEMPLATE') {
-      child = fixHTMLTemplateElement(child);
-    } else if (isSVGTemplate(child)) {
-      child = fixSVGTemplateElement(child);
-    }
-
     content.appendChild(child);
+  }
+
+  return template;
+}
+
+function fixHTMLTemplateElementRoot(template) {
+  let content = fixHTMLTemplateElement(template).content;
+  let childTemplates = content.querySelectorAll('template');
+
+  for (let i = 0, ii = childTemplates.length; i < ii; ++i) {
+    let child = childTemplates[i];
+
+    if (isSVGTemplate(child)) {
+      fixSVGTemplateElement(child);
+    } else {
+      fixHTMLTemplateElement(child);
+    }
   }
 
   return template;
@@ -44,5 +55,5 @@ function fixHTMLTemplateElement(template) {
 if (FEATURE.htmlTemplateElement) {
   FEATURE.ensureHTMLTemplateElement = function(template) { return template; };
 } else {
-  FEATURE.ensureHTMLTemplateElement = fixHTMLTemplateElement;
+  FEATURE.ensureHTMLTemplateElement = fixHTMLTemplateElementRoot;
 }
