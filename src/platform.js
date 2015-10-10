cef7ba8b5c4b8be9d6bb917913672c9aa108e652
Rob Eisenberg <rob@bluespire.com>
2015-10-10 13:57:29 -0400

    fix(platform): incorrect platform reference

--- src/platform.js
+++ src/platform.js
@@ -2,9 +2,9 @@ export const PLATFORM = {
   location: window.location,
   history: window.history,
   addEventListener(eventName: string, callback: Function, capture: boolean): void {
-    PLATFORM.global.addEventListener(eventName, callback, capture);
+    this.global.addEventListener(eventName, callback, capture);
   },
   removeEventListener(eventName: string, callback: Function, capture: boolean): void {
-    PLATFORM.global.removeEventListener(eventName, callback, capture);
+    this.global.removeEventListener(eventName, callback, capture);
   }
 };
