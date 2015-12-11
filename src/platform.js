8427b1f26c93033973cdfb91c4ec0c57ccf7326c
Jeremy Danyow <jdanyow@gmail.com>
2015-12-11 07:51:45 -0500

    feat(PAL): requestAnimationFrame and performance APIs

--- src/platform.js
+++ src/platform.js
@@ -6,5 +6,9 @@ export const _PLATFORM = {
   },
   removeEventListener(eventName: string, callback: Function, capture: boolean): void {
     this.global.removeEventListener(eventName, callback, capture);
+  },
+  performance: window.performance,
+  requestAnimationFrame(callback: Function): number {
+    return this.global.requestAnimationFrame(callback);
   }
 };
