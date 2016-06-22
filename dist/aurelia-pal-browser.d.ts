fdd3680c341fc9a031b4bfe0360d22cf28fdab62
Rob Eisenberg <rob@bluespire.com>
2016-06-22 08:31:26 -0400

    chore(all): update build

--- dist/aurelia-pal-browser.d.ts
+++ dist/aurelia-pal-browser.d.ts
@@ -1,19 +1,6 @@
 import {
   initializePAL
 } from 'aurelia-pal';
-export declare function _ensureFunctionName(): void;
-export declare function _ensureClassList(): void;
-export declare function _ensurePerformance(): void;
-export declare function _ensureCustomEvent(): void;
-export declare function _ensureElementMatches(): void;
-export declare const _FEATURE: any;
-export declare function _ensureHTMLTemplateElement(): void;
-
-/**
-* Represents the core APIs of the DOM.
-*/
-export declare const _DOM: any;
-export declare const _PLATFORM: any;
 
 /**
 * Initializes the PAL with the Browser-targeted implementation.
